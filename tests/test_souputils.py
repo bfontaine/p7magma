0d8d1dee5cdc1e1edd15890f88fc6c7897b5eeaa
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-21 14:26:46 +0100

    using unicode strings in tests

--- tests/test_souputils.py
+++ tests/test_souputils.py
@@ -22,32 +22,32 @@ class TestBase(unittest.TestCase):
     # .text
 
     def test_text_none(self):
-        self.assertEquals("", sp.text(None))
+        self.assertEquals(u"", sp.text(None))
 
     def test_text_strip(self):
-        self.assertEquals("hello", sp.text(FakeElement(" hello ")))
+        self.assertEquals(u"hello", sp.text(FakeElement(u" hello ")))
 
     def test_text_strip_newline(self):
-        self.assertEquals("hello", sp.text(FakeElement(" hello\n")))
+        self.assertEquals(u"hello", sp.text(FakeElement(u" hello\n")))
 
     def test_text_nostrip(self):
-        self.assertEquals(" hello ", sp.text(FakeElement(" hello "), False))
+        self.assertEquals(u" hello ", sp.text(FakeElement(u" hello "), False))
 
     # .coursecode
 
     def test_coursecode_none(self):
-        self.assertEquals("", sp.coursecode(None))
+        self.assertEquals(u"", sp.coursecode(None))
 
     def test_coursecode_strip(self):
-        self.assertEquals("tata", sp.coursecode(FakeElement(" tata")))
+        self.assertEquals(u"tata", sp.coursecode(FakeElement(u" tata")))
 
     def test_coursecode_strip_square_brackets(self):
-        self.assertEquals("abc", sp.coursecode(FakeElement("abc [123]")))
+        self.assertEquals(u"abc", sp.coursecode(FakeElement(u"abc [123]")))
 
     # .parse
 
     def test_parse_str_none(self):
-        self.assertEquals("", sp.parse(None, str))
+        self.assertEquals(u"", sp.parse(None, str))
 
     def test_parse_int_none(self):
         self.assertEquals(0, sp.parse(None, int))
@@ -59,45 +59,45 @@ class TestBase(unittest.TestCase):
         self.assertFalse(sp.parse(None, bool))
 
     def test_parse_str_empty_text(self):
-        self.assertEquals("", sp.parse(FakeElement(""), str))
+        self.assertEquals(u"", sp.parse(FakeElement(u""), str))
 
     def test_parse_int_empty_text(self):
-        self.assertEquals(0, sp.parse(FakeElement(""), int))
+        self.assertEquals(0, sp.parse(FakeElement(u""), int))
 
     def test_parse_float_empty_text(self):
-        self.assertEquals(0.0, sp.parse(FakeElement(""), float))
+        self.assertEquals(0.0, sp.parse(FakeElement(u""), float))
 
     def test_parse_bool_empty_text(self):
-        self.assertFalse(sp.parse(FakeElement(""), bool))
+        self.assertFalse(sp.parse(FakeElement(u""), bool))
 
     def test_parse_bool_spaces_only(self):
-        self.assertFalse(sp.parse(FakeElement("   \n"), bool))
+        self.assertFalse(sp.parse(FakeElement(u"   \n"), bool))
 
     def test_parse_int(self):
-        self.assertEquals(42, sp.parse(FakeElement(" 42"), int))
+        self.assertEquals(42, sp.parse(FakeElement(u" 42"), int))
 
     # .parseint
 
     def test_parseint(self):
-        self.assertEquals(42, sp.parseint(FakeElement("42\n")))
+        self.assertEquals(42, sp.parseint(FakeElement(u"42\n")))
 
     # .parsefloat
 
     def test_parsefloat(self):
-        self.assertEquals(3.14, sp.parsefloat(FakeElement(" 3.14")))
+        self.assertEquals(3.14, sp.parsefloat(FakeElement(u" 3.14")))
 
     # .parsebool
 
     def test_parsebool_oui(self):
-        self.assertTrue(sp.parsebool(FakeElement("oui ")))
-        self.assertTrue(sp.parsebool(FakeElement("\nOUI ")))
+        self.assertTrue(sp.parsebool(FakeElement(u"oui ")))
+        self.assertTrue(sp.parsebool(FakeElement(u"\nOUI ")))
 
     def test_parsebool_non(self):
-        self.assertFalse(sp.parsebool(FakeElement("non ")))
-        self.assertFalse(sp.parsebool(FakeElement(" NON")))
+        self.assertFalse(sp.parsebool(FakeElement(u"non ")))
+        self.assertFalse(sp.parsebool(FakeElement(u" NON")))
 
     def test_parsebool_int_true(self):
-        self.assertTrue(sp.parsebool(FakeElement("42")))
+        self.assertTrue(sp.parsebool(FakeElement(u"42")))
 
     def test_parsebool_int_false(self):
-        self.assertFalse(sp.parsebool(FakeElement("0")))
+        self.assertFalse(sp.parsebool(FakeElement(u"0")))
