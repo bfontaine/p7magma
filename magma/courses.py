51d30c3847ffdf1509f627b3b440472b2a2f3068
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-21 09:22:46 +0100

    course formatting

--- magma/courses.py
+++ magma/courses.py
@@ -1,7 +1,17 @@
 # -*- coding: UTF-8 -*-
 
+from souputils import text, coursecode, parseint, parsefloat
+
 class Course(dict):
-    pass
+
+    def __str__(self):
+        results = ''
+        if self['followed'] and self['session']:
+            results = ' --> %.2f/20 (%s)' % (self['result'], self['session'])
+
+        return '%s (%s, S%d) [%s, %.2f ECTS]%s' % (
+            self['title'], self['code'], self['semester'], self['status'],
+            self['ects'], results)
 
 
 class CoursesList(list):
@@ -17,13 +27,18 @@ class CoursesList(list):
         for tr in table.select('tr')[1:]:
             tds = tr.select('td')
             cs = Course(
-                    code=tds[0].text,
-                    name=tds[1].text,
-                    semester=tds[2].text,
-                    status=tds[3].text,
-                    ects=tds[4].text,
-                    followed=tds[5].text,
-                    score=tds[6].text,
-                    session=tds[7].text,
+                    code=coursecode(tds[0]),
+                    title=text(tds[1]),
+                    semester=parseint(tds[2]),
+                    status=text(tds[3]),
+                    ects=parsefloat(tds[4]),
+                    followed=text(tds[5]),
             )
+
+            if cs['followed']:
+                cs['result'] = parsefloat(tds[6])
+                cs['session'] = text(tds[7])
             self.append(cs)
+
+    def followed(self):
+        return filter(lambda x: x['followed'], self)
---
+++ magma/souputils.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+
+import re
+
+def text(el, strip=True):
+    if not el:
+        return ""
+
+    text = el.text
+    if strip:
+        text = text.strip()
+    return text.encode("utf-8")
+
+
+def coursecode(el):
+    txt = text(el)
+    return re.sub(r"\s*\[\d+\]$", "", txt)
+
+
+def parse(el, typ):
+    if not el:
+        return typ()
+    txt = text(el)
+    if not txt:
+        return typ()
+    return typ(txt)
+
+
+def parseint(el):
+    return parse(el, int)
+
+def parsefloat(el):
+    return parse(el, float)
+
+def parsebool(el):
+    txt = text(el)
+    up = txt.upper()
+    if up == u"OUI":
+        return True
+    if up == u"NON":
+        return False
+
+    return bool(parseint(el))
